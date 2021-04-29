c096d18a602446f4c44556c3bc312c7970edd666
botti.edoardo <botti.edoardo@TAVE-PC07.OSDBSESTO.local>
2021-04-29 12:21:10 +0200

    addarraychar

--- src/pongesort/PongeSort.java
+++ src/pongesort/PongeSort.java
@@ -15,7 +15,8 @@ public class PongeSort {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        char [] array1 = {'§','-',',','+','*','/','&','%','$','£','!'};
+        
     }
     
 }
